535166e152ea538b34e4143463e53b0007e7f76b
Xavier Paredes <jxavierpp1@gmail.com>
2022-06-22 18:32:20 +0000

    ✨ Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="rooftop_puzzle",
+    version="0.1.0",
+    packages=["rooftop_puzzle"],
+    author="Xavier Walls",
+    python_requires=">=3.10",
+)
